a92702cf652428297aaea7e342727c05980785d0
its-Simy <135281067+its-Simy@users.noreply.github.com>
2024-02-07 10:17:26 -0500

    Simple counter of objects using static Data Member

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        //Write a simple program that will show the count of the number of objects using the static data member.
+
+        
+
+    }
+}
